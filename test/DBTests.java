cb49b1782e29d391ba74af5235e39c81e3f25a49
Hampus Tågerud <hampus.tagerud@gmail.com>
2017-03-13 14:24:44 +0100

    All tests added

--- test/DBTests.java
+++ test/DBTests.java
@@ -4,4 +4,338 @@ import org.junit.Test;
  * Created by Hampus on 2017-03-13.
  */
 public class DBTests {
+    @Test
+    public void testDBConnection() {
+
+    }
+
+    @Test
+    public void testCreateUserEmpty() {
+
+    }
+
+    @Test
+    public void testCreateUserEmptyUsername() {
+
+    }
+
+    @Test
+    public void testCreateUserEmptyPassword() {
+
+    }
+
+    @Test
+    public void testCreateUserInvalidData() {
+
+    }
+
+    @Test
+    public void testCreateUserInvalidUsername() {
+
+    }
+
+    @Test
+    public void testCreateUserTakenUsername() {
+
+    }
+
+    @Test
+    public void testCreateUserInvalidPassword() {
+
+    }
+
+    @Test
+    public void testCreateUserValidData() {
+
+    }
+
+    @Test
+    public void testCreateReservationEmpty() {
+
+    }
+
+    @Test
+    public void testCreateReservationEmptyShowID() {
+
+    }
+
+    @Test
+    public void testCreateReservationEmptyCustomerID() {
+
+    }
+
+    @Test
+    public void testCreateReservationEmptySeatID() {
+
+    }
+
+    @Test
+    public void testCreateReservationInvalidShowID() {
+
+    }
+
+    @Test
+    public void testCreateReservationInvalidCustomerID() {
+
+    }
+
+    @Test
+    public void testCreateReservationInvalidSeat() {
+
+    }
+
+    @Test
+    public void testCreateReservationValidData() {
+
+    }
+
+    @Test
+    public void testCreateCustomerInvalidEmail() {
+
+    }
+
+    @Test
+    public void testCreateCustomerInvalidFirstname() {
+
+    }
+
+    @Test
+    public void testCreateCustomerInvalidPhonenumber() {
+
+    }
+
+    @Test
+    public void testCreateCustomerInvalidCreditcardNumber() {
+
+    }
+
+    @Test
+    public void testCreateCustomerEmptyEmail() {
+
+    }
+
+    @Test
+    public void testCreateCustomerEmptyFirstname() {
+
+    }
+
+    @Test
+    public void testCreateCustomerEmptyPhonenumber() {
+
+    }
+
+    @Test
+    public void testCreateCustomerEmptyCreditcardNumber() {
+
+    }
+
+    @Test
+    public void testCreateCustomerValidData() {
+
+    }
+
+    @Test
+    public void testRemoveReservationInvalidReservationID() {
+
+    }
+
+    @Test
+    public void testRemoveReservationInvalidUserID() {
+
+    }
+
+    @Test
+    public void testRemoveReservationInvalidSeatID() {
+
+    }
+
+    @Test
+    public void testRemoveReservationValidData() {
+
+    }
+
+    @Test
+    public void testChangeCustomerInvalidEmail() {
+
+    }
+
+    @Test
+    public void testChangeCustomerInvalidUsername() {
+
+    }
+
+    @Test
+    public void testChangeCustomerInvalidPassword() {
+
+    }
+
+    @Test
+    public void testChangeCustomerEmptyUsername() {
+
+    }
+
+    @Test
+    public void testChangeCustomerEmptyPassword() {
+
+    }
+
+    @Test
+    public void testChangeCustomerEmptyEmail() {
+
+    }
+
+    @Test
+    public void testChangeCustomerEmptyPhonenumber() {
+
+    }
+
+    @Test
+    public void testChangeCustomerInvalidPhonenumber() {
+
+    }
+
+    @Test
+    public void testChangeCustomerInvalidCreditcardNumber() {
+
+    }
+
+    @Test
+    public void testChangeCustomerEmptyCreditcardNumber() {
+
+    }
+
+    @Test
+    public void testChangeCustomerValidData() {
+
+    }
+
+    @Test
+    public void testSearchReservationInvalidReservationID() {
+
+    }
+
+    @Test
+    public void testSearchReservationValidData() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleEmptyTitle() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleInvalidTitle() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleInvalidPrice() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleInvalidLength() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleEmptyAgeLimit() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleInvalidAgeLimit() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleEmptyTime() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleInvalidTime() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleInvalidRoomID() {
+
+    }
+
+    @Test
+    public void testAddMovieScheduleValidData() {
+
+    }
+
+    @Test
+    public void testCreateAdminUserInvalidUsername() {
+
+    }
+
+    @Test
+    public void testCreateAdminUserEmptyUsername() {
+
+    }
+
+    @Test
+    public void testCreateAdminUserInvalidPassword() {
+
+    }
+
+    @Test
+    public void testCreateAdminUserEmptyPassword() {
+
+    }
+
+    @Test
+    public void testCreateAdminUserValidData() {
+
+    }
+
+    @Test
+    public void testCheckReservationInvalidID() {
+
+    }
+
+    @Test
+    public void testCheckReservationValidData() {
+
+    }
+
+    @Test
+    public void testChangeReservationInvalidShowID() {
+
+    }
+
+    @Test
+    public void testChangereservationEmptyShowID() {
+
+    }
+
+    @Test
+    public void testChangeReservationInvalidCustomerID() {
+
+    }
+
+    @Test
+    public void testChangeReservationEmptyCustomerID() {
+
+    }
+
+    @Test
+    public void testChangeReservationInvalidSeatID() {
+
+    }
+
+    @Test
+    public void testChangeReservationEmptySeatID() {
+
+    }
+
+    @Test
+    public void testChangeReservationValidData() {
+
+    }
 }
